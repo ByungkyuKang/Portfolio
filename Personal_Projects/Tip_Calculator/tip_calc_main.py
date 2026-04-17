ee328f9442c7416dc18e98f9136865a810d4f9bf
ByungkyuKang <byungkyukang702@gmail.com>
2026-04-17 11:21:25 -0400

    Update tip_calc_main.py

--- Personal_Projects/Tip_Calculator/tip_calc_main.py
+++ Personal_Projects/Tip_Calculator/tip_calc_main.py
@@ -207,4 +207,4 @@ app = flet_fastapi.app(
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8080))
-    uvicorn.run("tip_calc_main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
